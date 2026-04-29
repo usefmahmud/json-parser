27ad6fee8e711f096a6e76bcc49b00db4510f515
Yousef Mahmoud <usef.mahmoud@outlook.com>
2026-04-29 20:01:42 +0300

    feat: implement tokenization logic for various token types in Lexer

--- src/parser/lexer.ts
+++ src/parser/lexer.ts
@@ -45,8 +45,16 @@ export class Lexer {
     return this.position >= this.input.length;
   }
 
+  private is_alpha(char: string): boolean {
+    return /[a-zA-Z]/.test(char);
+  }
+
   private is_digit(char: string): boolean {
     return /^\d$/.test(char);
+  }
+
+  private is_keyword(str: string): boolean {
+    return str in Keywords;
   }
 
   private skip_whitespace(): void {
@@ -76,4 +84,67 @@ export class Lexer {
     }
     return result;
   }
+
+  public tokenize(): TokenValue[] {
+    const tokens: TokenValue[] = [];
+
+    while (!this.is_at_end()) {
+      this.skip_whitespace();
+
+      const char = this.peek();
+
+      if (char === "{") {
+        tokens.push(this.token(Token.LEFT_BRACE));
+        this.advance();
+      } else if (char === "}") {
+        tokens.push(this.token(Token.RIGHT_BRACE));
+        this.advance();
+      } else if (char === "[") {
+        tokens.push(this.token(Token.LEFT_BRACKET));
+        this.advance();
+      } else if (char === "]") {
+        tokens.push(this.token(Token.RIGHT_BRACKET));
+        this.advance();
+      } else if (char === ":") {
+        tokens.push(this.token(Token.COLON));
+        this.advance();
+      } else if (char === ",") {
+        tokens.push(this.token(Token.COMMA));
+        this.advance();
+      } else if (char === '"') {
+        tokens.push(this.token(Token.STRING, this.read_string()));
+      } else if (this.is_digit(char)) {
+        tokens.push(this.token(Token.NUMBER, this.read_number()));
+      } else if (this.is_alpha(char)) {
+        let word = "";
+
+        while (!this.is_at_end() && this.is_alpha(this.peek())) {
+          word += this.peek();
+          this.advance();
+        }
+
+        if (this.is_keyword(word)) {
+          tokens.push(this.token(Keywords[word as keyof typeof Keywords]));
+        } else {
+          throw new Error(`Unexpected Token: ${word}`);
+        }
+      } else {
+        throw new Error(`Unexpected Token at: ${char}`);
+      }
+    }
+
+    return tokens;
+  }
+
+  private token(type: Token, value?: string): TokenValue {
+    return { type, value };
+  }
 }
+
+const input =
+  '{"name": "yousef", "age": 21, "isActive": true, "skills": ["skill1", "skill2"]}';
+const lexer = new Lexer(input);
+const tokens = lexer.tokenize();
+console.log(
+  tokens.map((token) => ({ type: Token[token.type], value: token.value })),
+);
