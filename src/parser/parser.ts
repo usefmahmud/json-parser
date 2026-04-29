6316d47794ec4fd555b8bb292c5342821a9a211f
Yousef Mahmoud <usef.mahmoud@outlook.com>
2026-04-30 00:34:03 +0300

    refactor: improve token consumption and value parsing in Parser class

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -14,30 +14,38 @@ export class Parser {
     this.position++;
   }
 
-  private consume(tokenType: TokenValue["type"]) {
-    if (this.peek().type !== tokenType) {
-      throw new Error("Unexpected Token Type");
+  private consume(type: Token): TokenValue {
+    const token = this.peek();
+
+    if (token.type !== type) {
+      throw new Error(`Expected ${Token[type]} but got ${Token[token.type]}`);
     }
 
     this.advance();
+    return token;
   }
 
-  private is_at_end() {
+  private is_at_end(): boolean {
     return this.position >= this.tokens.length;
   }
 
-  private parse_value() {
+  private parse_value(): AST {
     const token = this.peek();
 
     if (token.type === Token.STRING) {
-      return this.token("String", token.value);
+      const t = this.consume(Token.STRING);
+      return this.token("String", t.value!);
     } else if (token.type === Token.NUMBER) {
-      return this.token("Number", Number(token.value));
+      const t = this.consume(Token.NUMBER);
+      return this.token("Number", Number(t.value));
     } else if (token.type === Token.TRUE) {
+      this.consume(Token.TRUE);
       return this.token("Boolean", true);
     } else if (token.type === Token.FALSE) {
+      this.consume(Token.FALSE);
       return this.token("Boolean", false);
     } else if (token.type === Token.NULL) {
+      this.consume(Token.NULL);
       return this.token("Null", null);
     } else if (token.type === Token.LEFT_BRACE) {
       // parse object
@@ -52,9 +60,6 @@ export class Parser {
     type: T,
     value: Extract<AST, { type: T }>["value"],
   ): Extract<AST, { type: T }> {
-    return {
-      type,
-      value,
-    } as Extract<AST, { type: T }>;
+    return { type, value } as Extract<AST, { type: T }>;
   }
 }
